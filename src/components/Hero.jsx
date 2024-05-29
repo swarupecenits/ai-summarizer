3587d24c16488063b63122cc10d01ce34ee374fe
Swarup Chanda <swarupchanda1963@gmail.com>
2024-05-29 21:26:23 +0530

    Updarted

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,23 @@
-import {logo} from '../assets'
+import { logo } from '../assets'
 
 const Hero = () => {
-  return (
-    <header className='w-full flex justify-center items-center flex-col'>
-        <nav className='flex justify-between items-center flex-col'>
-            <img src={logo} alt="sumz-logo" className='w-28 object-contain' />
-            <button type='button' onClick={()=> window.open('')} className=''>
-                Github
-            </button>
-        </nav>
-    </header>
-  )
+    return (
+        <header className='w-full flex justify-center items-center flex-col'>
+            <nav className='flex justify-between items-center w-full mb-10 pt-4'>
+                <img src={logo} alt="sumz-logo" className='w-28 object-contain' />
+                <button type='button' onClick={() => window.open('https://github.com/swarupecenits/ai-summarizer.git')} className="black_btn">
+                    Github
+                </button>
+            </nav>
+            <h1 className='head_text'>
+                Summarize Articles with <br className='max-md:hidden' />
+                <span className='orange_gradient '>OpenAI GPT-4</span>
+            </h1>
+            <h2 className='desc'>
+                "Make reading easier with Summarize, an open-source tool that transforms lengthy articles into clear and concise summaries
+            </h2>
+        </header>
+    )
 }
 
 export default Hero
